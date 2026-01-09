bf57e2c3fe8a39d77b2297abeb051e41a6a8d06e
Ray Lau <ray.lau@lowrisc.org>
2026-01-09 14:25:05 +0000

    [sw] Add PLIC smoketest

--- sw/device/lib/hal/uart.h
+++ sw/device/lib/hal/uart.h
@@ -5,6 +5,22 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
+
+#define UART_INTR_STATE_REG  (0x0)
+#define UART_INTR_ENABLE_REG (0x4)
+#define UART_INTR_TEST_REG   (0x8)
+
+#define UART_INTR_TX_WATERMARK  (0)
+#define UART_INTR_RX_WATERMARK  (1)
+#define UART_INTR_TX_DONE       (2)
+#define UART_INTR_RX_OVERFLOW   (3)
+#define UART_INTR_RX_FRAME_ERR  (4)
+#define UART_INTR_RX_BREAK_ERR  (5)
+#define UART_INTR_RX_TIMEOUT    (6)
+#define UART_INTR_RX_PARITY_ERR (7)
+#define UART_INTR_TX_EMPTY      (8)
+#define UART_MAX_INTR           (8)
 
 #define UART_CTRL_REG   (0x10)
 #define UART_CTRL_SLPBK (4)
@@ -25,6 +41,11 @@ typedef void *uart_t;
 
 #define UART_FROM_BASE_ADDR(addr) ((uart_t)(addr))
 
+void uart_interrupt_disable_all(uart_t uart);
+void uart_interrupt_enable(uart_t uart, uint8_t intr_id);
+void uart_interrupt_disable(uart_t uart, uint8_t intr_id);
+void uart_interrupt_trigger(uart_t uart, uint8_t intr_id);
+void uart_interrupt_clear(uart_t uart, uint8_t intr_id);
 int uart_init(uart_t uart);
 void uart_enable_rx_int(void);
 int uart_in(uart_t uart);
---
+++ sw/device/tests/plic/smoketest.c
@@ -0,0 +1,146 @@
+// Copyright lowRISC contributors (COSMIC project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "hal/mocha.h"
+#include "hal/plic.h"
+#include "hal/uart.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+uint64_t csr_mip_get()
+{
+    uint64_t mip;
+    __asm__ volatile("csrr %0, mip\n\t" : "=r"(mip));
+    return mip;
+}
+
+bool reg_test(plic_t plic)
+{
+    plic_init(plic);
+
+    plic_interrupt_priority_set(plic, 4, 2);
+    if ((plic_interrupt_priority_get(plic, 4) & PLIC_PRIO_MASK) != 2) {
+        return false;
+    }
+
+    plic_machine_interrupt_enable(plic, 22);
+    if (!plic_machine_interrupt_enable_get(plic, 22)) {
+        return false;
+    }
+
+    plic_machine_interrupt_disable(plic, 22);
+    if (plic_machine_interrupt_enable_get(plic, 22)) {
+        return false;
+    }
+
+    plic_supervisor_interrupt_enable(plic, 13);
+    if (!plic_supervisor_interrupt_enable_get(plic, 13)) {
+        return false;
+    }
+
+    plic_supervisor_interrupt_disable(plic, 13);
+    if (plic_supervisor_interrupt_enable_get(plic, 13)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool uart_machine_irq_test(plic_t plic, uart_t uart)
+{
+    uint8_t intr_id;
+
+    const int MIP_RD_RETRY_COUNT = 20;
+    const int UART_INTR_ID = 8;
+    const uint64_t MEIP_MASK = (1 << 11);
+
+    plic_init(plic);
+    plic_interrupt_priority_set(plic, UART_INTR_ID, 3);
+    plic_machine_priority_threshold_set(plic, 0);
+
+    uart_interrupt_disable_all(uart);
+    uart_interrupt_enable(uart, UART_INTR_RX_FRAME_ERR);
+
+    plic_machine_interrupt_enable(plic, UART_INTR_ID);
+
+    // Check that mip MEIP is clear
+    if ((csr_mip_get() & MEIP_MASK) != 0) {
+        return false;
+    }
+
+    uart_interrupt_trigger(uart, UART_INTR_RX_FRAME_ERR);
+
+    // Check that mip MEIP is set
+    // Retry to give time for mip to be updated
+    for (int i = 0; (csr_mip_get() & MEIP_MASK) == 0 && i < MIP_RD_RETRY_COUNT; ++i) {
+    }
+
+    if ((csr_mip_get() & MEIP_MASK) == 0) {
+        return false;
+    }
+
+    intr_id = plic_machine_interrupt_claim(plic);
+    uart_interrupt_clear(uart, UART_INTR_RX_FRAME_ERR);
+    plic_machine_interrupt_complete(plic, intr_id);
+
+    // Check that mip MEIP is clear
+    if ((csr_mip_get() & MEIP_MASK) != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool uart_supervisor_irq_test(plic_t plic, uart_t uart)
+{
+    uint8_t intr_id;
+
+    const int MIP_RD_RETRY_COUNT = 20;
+    const int UART_INTR_ID = 8;
+    const uint64_t SEIP_MASK = (1 << 9);
+
+    plic_init(plic);
+    plic_interrupt_priority_set(plic, UART_INTR_ID, 3);
+    plic_supervisor_priority_threshold_set(plic, 0);
+
+    uart_interrupt_disable_all(uart);
+    uart_interrupt_enable(uart, UART_INTR_RX_TIMEOUT);
+
+    plic_supervisor_interrupt_enable(plic, UART_INTR_ID);
+
+    // Check that mip SEIP is clear
+    if ((csr_mip_get() & SEIP_MASK) != 0) {
+        return false;
+    }
+
+    uart_interrupt_trigger(uart, UART_INTR_RX_TIMEOUT);
+
+    // Check that mip SEIP is set
+    // Retry to give time for mip to be updated
+    for (int i = 0; (csr_mip_get() & SEIP_MASK) == 0 && i < MIP_RD_RETRY_COUNT; ++i) {
+    }
+
+    if ((csr_mip_get() & SEIP_MASK) == 0) {
+        return false;
+    }
+
+    intr_id = plic_supervisor_interrupt_claim(plic);
+    uart_interrupt_clear(uart, UART_INTR_RX_TIMEOUT);
+    plic_supervisor_interrupt_complete(plic, intr_id);
+
+    // Check that mip SEIP is clear
+    if ((csr_mip_get() & SEIP_MASK) != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool test_main(uart_t console)
+{
+    plic_t plic = mocha_system_plic();
+
+    return reg_test(plic) && uart_machine_irq_test(plic, console) &&
+           uart_supervisor_irq_test(plic, console);
+}
