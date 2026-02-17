fea348ac1539fa67b56f4441a5adceb49fc435b7
martin-velay <mvelay@lowrisc.org>
2026-02-17 18:17:06 +0100

    [top,dv] Manage SPI device interface
    - Add SPI agent to the UVM env
    
    Signed-off-by: martin-velay <mvelay@lowrisc.org>

--- hw/top_chip/dv/env/top_chip_dv_env_pkg.sv
+++ hw/top_chip/dv/env/top_chip_dv_env_pkg.sv
@@ -9,6 +9,7 @@ package top_chip_dv_env_pkg;
   import mem_bkdr_util_pkg::*;
   import sw_test_status_pkg::*;
   import uart_agent_pkg::*;
+  import spi_agent_pkg::*;
 
   // Macro includes
   `include "uvm_macros.svh"
