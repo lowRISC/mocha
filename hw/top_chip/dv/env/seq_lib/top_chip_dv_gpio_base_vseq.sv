021fc900447d72b18bb323de90ade18c29caa7d8
Kinza Qamar <kqzaman@lowrisc.org>
2026-03-20 15:55:35 +0000

    [chip, gpio, dv] Added gpio_base_vseq
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

---
+++ hw/top_chip/dv/env/seq_lib/top_chip_dv_gpio_base_vseq.sv
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors (COSMIC project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class top_chip_dv_gpio_base_vseq extends top_chip_dv_base_vseq;
+  `uvm_object_utils(top_chip_dv_gpio_base_vseq)
+
+  // Standard SV/UVM methods
+  extern function new(string name = "");
+  extern task body();
+endclass : top_chip_dv_gpio_base_vseq
+
+function top_chip_dv_gpio_base_vseq::new (string name = "");
+  super.new(name);
+endfunction : new
+
+task top_chip_dv_gpio_base_vseq::body();
+  super.body();
+  `DV_WAIT(cfg.sw_test_status_vif.sw_test_status == SwTestStatusInTest);
+  // TODO
+endtask : body
