46e24de289fe02fad4a38f395a6820cee7185142
Kolos Koblasz <kolos.koblasz@semify-eda.com>
2026-04-01 09:27:25 +0100

    [rom_ctrl, test] rom_ctrl smoketest added
     * new testcase implemented among top level sw tests
     * the new test reads the compile time initialized ROM values
     * if any comparision fails (expected values differs from read value)
       test is terminated with an error
     * sw test files reformated to complie with clang format
     * fixed issues highlighted during PR review
    
    Signed-off-by: Kolos Koblasz <kolos.koblasz@semify-eda.com>

---
+++ sw/device/lib/hal/rom_ctrl.c
@@ -0,0 +1,14 @@
+// Copyright lowRISC contributors (COSMIC project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "hal/rom_ctrl.h"
+#include "hal/mmio.h"
+#include "hal/mocha.h"
+#include <stdint.h>
+
+/* Read a 32 bit data word from ROM memory */
+uint32_t read_rom(rom_t rom, uint32_t rel_addr)
+{
+    return DEV_READ(rom + rel_addr);
+}
