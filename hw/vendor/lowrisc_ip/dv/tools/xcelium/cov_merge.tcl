f7439dede01550a212b0b2bb29ba0825b7384026
Kinza Qamar <kqzaman@lowrisc.org>
2026-04-20 10:30:49 +0100

    Update lowrisc_ip to lowRISC/opentitan@1661a0f774
    Update code from upstream repository
    https://github.com/lowRISC/opentitan to revision
    1661a0f774db8862d38c9f6af94f551176efe27a
    
    * [reggen] Fix comment in reg_top.sv.tpl (Rupert Swarbrick)
    * [reggen,doc] Slightly extend partial write error description (Rupert
      Swarbrick)
    * [xbar,dv] Fix covergroups namespace collision (martin-velay)
    * [script] Fix Xcelium ranking report generation (martin-velay)
    * [script] Remove double entry of exclusion (martin-velay)
    * [jtag,dv] Avoid going to Run-Test/Idle when unnecessary (Rupert
      Swarbrick)
    * [I2C, DV, RTL] Remove a non-existent assertion from DV / RTL (Kinza
      Qamar)
    * [dv] Remove unused dv_vif_wrap class (Rupert Swarbrick)
    * [dv] Check frequency later in clk_rst_if (Rupert Swarbrick)
    * [rom_ctrl,rtl] Add simple consistency check for kmac_data_o.last
      (Rupert Swarbrick)
    * [dv] Avoid null pointer dereference in dv_base_scoreboard (Rupert
      Swarbrick)
    * [tlul,assert] Fix hierarchical path in uvm_config_db (Rupert
      Swarbrick)
    * [rom_ctrl,fpv,rtl] Rephrase two assertions to avoid cover properties
      (Rupert Swarbrick)
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/vendor/lowrisc_ip/dv/tools/xcelium/cov_merge.tcl
+++ hw/vendor/lowrisc_ip/dv/tools/xcelium/cov_merge.tcl
@@ -18,13 +18,9 @@ puts "Output directory for merged coverage:"
 set cov_merge_db_dir [string trim $::env(cov_merge_db_dir) " \"'"]
 
 # Run the merge command.
-if { [info exists ::env(cov_db_runfile)] } {
-  merge -runfile $::env(cov_db_runfile) -out $cov_merge_db_dir -overwrite -initial_model union_all
-} else {
-  merge $cov_db_dirs -out $cov_merge_db_dir -overwrite -initial_model union_all
-}
+merge $cov_db_dirs -out $cov_merge_db_dir -overwrite -initial_model union_all
 
 # Create a file with the path to the cover dirs
-set filepointer [open "$cov_merge_db_dir/runs.txt" w]
-puts $filepointer "$cov_db_dirs"
-close $filepointer
+# Replace spaces with newlines so each run is on its own line for the 'rank' command
+set cov_db_dirs_rank [string map {" " "\n"} [string trim $cov_db_dirs]]
+exec sh -c "cat << 'EOF' > $cov_merge_db_dir/runs.txt\n$cov_db_dirs_rank\nEOF"
