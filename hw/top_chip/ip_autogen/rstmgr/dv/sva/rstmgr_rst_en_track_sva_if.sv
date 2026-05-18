f56675d77463011c67fd49940e735a69c6c653f9
Kolos Koblasz <kolos.koblasz@semify-eda.com>
2026-05-18 17:40:41 +0100

    [rstmgr_dv, autogen] Autogenerated code added
     * As result of previous patches to template files
       new dv files are generated for rstmgr.
    
    Signed-off-by: Kolos Koblasz <kolos.koblasz@semify-eda.com>

--- hw/top_chip/ip_autogen/rstmgr/dv/sva/rstmgr_rst_en_track_sva_if.sv
+++ hw/top_chip/ip_autogen/rstmgr/dv/sva/rstmgr_rst_en_track_sva_if.sv
@@ -13,7 +13,7 @@ interface rstmgr_rst_en_track_sva_if (
   input logic rst_por_ni
 );
   import rstmgr_pkg::DomainAonSel;
-  import rstmgr_pkg::Domain0Sel;
+  import rstmgr_pkg::DomainMainSel;
   localparam int DELAY = 1;
 
   `ASSERT(D0RstPorAonEnTracksRstPorAonActive_A,
