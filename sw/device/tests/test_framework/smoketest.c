5d5185f96edf36864806c7303e4fb5e2c44f84c9
martin-velay <mvelay@lowrisc.org>
2026-06-04 11:32:32 +0200

    [sw] Add HW_ID platform check
    Update the test framework smoketest to read the HW_ID register,
    validate it against known values, and fail on an unrecognised ID.
    
    Co-authored-by: Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>
    Signed-off-by: martin-velay <mvelay@lowrisc.org>

--- sw/device/tests/test_framework/smoketest.c
+++ sw/device/tests/test_framework/smoketest.c
@@ -2,11 +2,32 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "hal/dv.h"
+#include "hal/mocha.h"
 #include "hal/uart.h"
 #include <stdbool.h>
 
 bool test_main(uart_t console)
 {
+    dv_window_t dv_window = mocha_system_dv_window();
+    uint32_t hwid = DEV_READ(&dv_window->hw_id);
+
     uart_puts(console, "Test framework smoketest\n");
+
+    switch (hwid) {
+    case dv_hwid_fpga_genesys2:
+        uart_puts(console, "Platform: Genesys2 FPGA\n");
+        break;
+    case dv_hwid_sim_verilator:
+        uart_puts(console, "Platform: Verilator Simulation\n");
+        break;
+    case dv_hwid_sim_uvm:
+        uart_puts(console, "Platform: UVM Simulation\n");
+        break;
+    default:
+        uart_puts(console, "Unknown Hardware ID\n");
+        return false;
+    }
+
     return true;
 }
